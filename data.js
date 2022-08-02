04ef02144a63abc24f51228e412eb6a59281ef05
Kristy Nguyen <kristypnguyen7@gmail.com>
2022-08-02 16:07:16 -0600

    Set up initial configurations

---
+++ data.js
@@ -0,0 +1,7 @@
+const shops = [
+  { id: 1, name: 'Sharetea', region: 'West Denver', address: '424 S Teller St, Lakewood, CO 80226', description: '' },
+  { id: 2, name: 'Boba Chic', region: 'Southwest Denver', address: '655 S Federal Blvd, Denver, CO 80219', description: '' },
+  { id: 3, name: 'KUTEA', region: 'Southwest Denver', address: '1147 S Federal Blvd, Denver, CO 80219', description: '' },
+]
+
+module.exports = shops; 
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const shops = require('./data.js');
+
+app.use(cors());
+app.use(express.json());
+
+app.locals.title = 'Boba Buddy API';
+app.locals.shops = shops;
+
+app.set('port', 3001);
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
+});
+
+app.get('/', (request, response) => {
+  response.status(200).json(app.locals.shops);
+});
